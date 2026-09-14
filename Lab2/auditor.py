16972cee6d13d1ce3dde108f510b70e5c86efe8a
Jeremy <lokejeremy1@gmail.com>
2026-09-15 01:38:33 +0800

    Final fix

--- Lab2/auditor.py
+++ Lab2/auditor.py
@@ -3,17 +3,13 @@ total_inventory = 0
 rej_inventory = 0
 
 while True:
-    inventory = (input("Enter the number of items in inventory (or type 'quit' to exit): "))
+    inventory = input("Enter the number of items in inventory (or type 'quit' to exit): ")
     if inventory.isdigit():  
         total_inventory += int(inventory)
-        if int(total_inventory) >500:
+        if total_inventory >500:
             print("Alert: Inventory exceeds 500 items. Please check your stock.")
             break
-        elif int(inventory) < 0:
-            rej_inventory += 1
-            print("Inventory cannot be negative. Please enter a valid number.")
-            total_inventory -= int(inventory)
-        elif int(inventory) >= 0 and int(total_inventory) <= 500:
+        else:
             print("You have", total_inventory, "items in inventory.")
     elif inventory == "quit":
         print("Exiting... Total Units Processed is", total_inventory, "items.")
